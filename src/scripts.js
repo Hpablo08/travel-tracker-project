97e78d4b3dc5accfe015ef2bba110e14445b7abe
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-09-23 09:19:30 -0600

    Add HTML and CSS

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,15 @@
 import { fetchData, postData } from './apiCalls';
 import './css/styles.css';
+import Repository from './repository';
+import Session from './session';
+
+// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import './images/turing-logo.png'
 // GLOBAL DATA ***************************************************
 
 
 // FETCH DATA *****************************************************
+
 Promise.all([fetchData("travelers"), fetchData("trips"), fetchData("destinations")])
   .then((repos) => {
     setData(repos);
